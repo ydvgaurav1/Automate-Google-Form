9d065041c600f01f0305d59171f657e702e405ae
Gaurav Yadav <gauravlove872@gmail.com>
2025-01-13 13:15:30 +0530

    ME_QA_XGOOGLE_FORM_MODULE_L1

--- src/test/java/demo/TestCases.java
+++ src/test/java/demo/TestCases.java
@@ -1,34 +1,38 @@
 package demo;
 
+import java.time.Duration;
+import java.util.logging.Level;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.logging.LogType;
 import org.openqa.selenium.logging.LoggingPreferences;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-import java.util.logging.Level;
-// import io.github.bonigarcia.wdm.WebDriverManager;
 import demo.wrappers.Wrappers;
 
 public class TestCases {
     ChromeDriver driver;
+    Wrappers wrapper;
 
     /*
-     * TODO: Write your tests here with testng @Test annotation. 
-     * Follow `testCase01` `testCase02`... format or what is provided in instructions
+     * TODO: Write your tests here with testng @Test annotation.
+     * Follow `testCase01` `testCase02`... format or what is provided in
+     * instructions
      */
 
-     
     /*
-     * Do not change the provided methods unless necessary, they will help in automation and assessment
+     * Do not change the provided methods unless necessary, they will help in
+     * automation and assessment
      */
     @BeforeTest
-    public void startBrowser()
-    {
+    public void startBrowser() {
         System.setProperty("java.util.logging.config.file", "logging.properties");
 
         // NOT NEEDED FOR SELENIUM MANAGER
@@ -42,16 +46,61 @@ public class TestCases {
         options.setCapability("goog:loggingPrefs", logs);
         options.addArguments("--remote-allow-origins=*");
 
-        System.setProperty(ChromeDriverService.CHROME_DRIVER_LOG_PROPERTY, "build/chromedriver.log"); 
+        System.setProperty(ChromeDriverService.CHROME_DRIVER_LOG_PROPERTY, "build/chromedriver.log");
 
         driver = new ChromeDriver(options);
+        wrapper = new Wrappers(driver);
 
         driver.manage().window().maximize();
     }
 
+    @Test
+    public void testCase01() throws InterruptedException {
+        System.out.println("Start TestCase 01");
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        // Navigating to the Google Form URL
+        wrapper.navigateToUrl(
+                "https://docs.google.com/forms/d/e/1FAIpQLSep9LTMntH5YqIXa5nkiPKSs283kdwitBBhXWyZdAS-e4CxBQ/viewform");
+
+        wrapper.fillTextField(By.xpath("(//input[@type='text'])[1]"), "Crio Learner");
+
+        // Generating unique text for the TextArea field
+        long epochTime = System.currentTimeMillis();
+        wrapper.fillTextField(By.xpath("//textarea[@class='KHxj8b tL9Q4c']"),
+                "I want to be the best QA Engineer!" + epochTime);
+
+        wrapper.clickElement(By.xpath("(//div[@class=\"Od2TWd hYsg7c\"])[3]"));
+
+        // Using WebDriverWait to ensure elements are clickable before interacting
+        System.out.println("Using WebDriverWait to ensure the elements are clickable before interacting with them");
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@data-answer-value=\"Java\"]"))).click();
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@data-answer-value=\"Selenium\"]"))).click();
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@data-answer-value=\"TestNG\"]"))).click();
+
+        wrapper.clickElement(By.xpath("//span[text()=\"Choose\"]"));
+        Thread.sleep(5000);
+        wrapper.clickElement(By.xpath("(//span[@class='vRMGwf oJeWuf'][text()='Mr'])[2]"));
+
+        String dateMinus7Days = wrapper.getDateMinusDays(7);
+        wrapper.fillTextField(By.xpath("//input[contains(@type,'date')]"), dateMinus7Days);
+
+        wrapper.fillTextField(By.xpath("//input[@aria-label='Hour']"), "07");
+        wrapper.fillTextField(By.xpath("//input[@aria-label='Minute']"), "30");
+
+        wrapper.clickElement(By.xpath("//div[@aria-label='Submit']"));
+
+        Thread.sleep(5000);
+
+        // Using WebDriverWait to ensure elements are clickable before interacting
+        String successMsg = wrapper.printText(By.xpath("//div[@class='vHW8K']"));
+        System.out.println(successMsg);
+        // Ending the test case
+        System.out.println("End TestCase 01");
+    }
+
     @AfterTest
-    public void endTest()
-    {
+    public void endTest() {
         driver.close();
         driver.quit();
 
--- src/test/java/demo/wrappers/Wrappers.java
+++ src/test/java/demo/wrappers/Wrappers.java
@@ -1,11 +1,13 @@
 package demo.wrappers;
 
 import java.time.Instant;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -13,7 +15,36 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import java.time.Duration;
 
 public class Wrappers {
-    /*
-     * Write your selenium wrappers here
-     */
+    WebDriver driver;
+    WebDriverWait wait;
+    
+        public Wrappers(WebDriver driver) {
+            this.driver = driver;
+            this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    }
+
+    public void navigateToUrl(String url){
+        driver.get(url);
+    }
+
+    public void fillTextField(By locator,String text){
+        WebElement element = wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+        element.sendKeys(text);
+    }
+
+    public void clickElement(By locator){
+        WebElement element = wait.until(ExpectedConditions.elementToBeClickable(locator));
+        element.click();
+    }
+
+    public String printText(By locator){
+        WebElement element = wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+        return element.getText();
+    }
+
+    public String getDateMinusDays(int days) {
+        LocalDate date = LocalDate.now().minusDays(days);
+        return date.format(DateTimeFormatter.ofPattern("dd-MM-yyyy"));
+    }
+
 }
